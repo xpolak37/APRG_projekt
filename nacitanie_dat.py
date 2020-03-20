112c63721ed46c252e1a28bb538a434f100751d9
Petra Polakovicova <xpolak37@vutbr.cz>
2020-03-20 21:15:19 +0100

    Rozdelenie suradnic podla objektov

--- nacitanie_dat.py
+++ nacitanie_dat.py
@@ -4,6 +4,8 @@ with open ("vstupne_udaje/map_data_0.json") as file:
     dictionary =json.load(file)
 
 objects = dictionary.pop("object")
+x_coordinates1 = []
+y_coordinates1 = []
 x_coordinates = []
 y_coordinates = []
 
@@ -12,8 +14,11 @@ for object in objects:
     for numbers in coordinates:
         x_coordinate = numbers[0]
         y_coordinate = numbers[1]
-        x_coordinates.append(x_coordinate)
-        y_coordinates.append(y_coordinate)
+        x_coordinates1.append(x_coordinate)
+        y_coordinates1.append(y_coordinate)
+
+    x_coordinates.append(x_coordinates1)
+    y_coordinates.append(y_coordinates1)
 
 print(x_coordinates)
 print(y_coordinates)
