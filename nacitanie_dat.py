80299786ad87d0bce691a3214fb1b3eec65957ed
Petra Polakovicova <xpolak37@vutbr.cz>
2020-03-21 14:45:55 +0100

    funkcie start_position a end_position

--- nacitanie_dat.py
+++ nacitanie_dat.py
@@ -1,8 +1,12 @@
 import json
 
 with open ("vstupne_udaje/map_data_0.json") as file:
-    dictionary =json.load(file)
+    dictionary = json.load(file)
 
+with open ("vstupne_udaje/test_path.json") as file2:
+    dictionary2 = json.load(file2)
+
+path_number = 0
 
 def x_coordinates():
     objects = dictionary["object"]
@@ -22,7 +26,7 @@ def x_coordinates():
 
 
 def y_coordinates():
-    objects = dictionary.pop("object")
+    objects = dictionary["object"]
     y_coordinates = []
 
     for object in objects:
@@ -35,3 +39,17 @@ def y_coordinates():
         y_coordinates.append(y_coordinates1)
 
     return(y_coordinates)
+
+
+def start_position():
+    paths = dictionary2["path"]
+    our_path = paths[path_number]
+    start_position = our_path["start"]
+    return(start_position)
+
+
+def end_position():
+    paths = dictionary2["path"]
+    our_path = paths[path_number]
+    end_position = our_path["end"]
+    return (end_position)
