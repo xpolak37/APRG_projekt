c1df85b1719923ef6f141a87b991329cf990df3c
Petra Polakovicova <xpolak37@vutbr.cz>
2020-04-29 17:16:18 +0200

    funkcia main

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import json
+import zobrazenie_dat
 
 with open("vstupne_udaje/map_data_1.json") as map_file:  # toto skusit dat prec dat do config file
     map_dictionary = json.load(map_file)
@@ -6,3 +7,9 @@ with open("vstupne_udaje/map_data_1.json") as map_file:  # toto skusit dat prec 
 with open("vstupne_udaje/test_path.json") as path_file:
     path_dictionary = json.load(path_file)
     path_index = 0
+
+def main():
+    zobrazenie_dat.graph()
+
+if __name__ == "__main__":
+    main()
