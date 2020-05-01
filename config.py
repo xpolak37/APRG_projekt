ca37c6d18e325b0e96267837f782ac9f7020731a
Petra Polakovicova <xpolak37@vutbr.cz>
2020-05-01 12:11:12 +0200

    odstranenie main z config

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import json
-import zobrazenie_dat
 
 #nastavenie mapy s prekazkami a cesty
 
@@ -9,9 +8,3 @@ with open("vstupne_udaje/map_data_1.json") as map_file:
 with open("vstupne_udaje/test_path.json") as path_file:
     path_dictionary = json.load(path_file)
     path_index = 0
-
-def main():
-    zobrazenie_dat.graph()
-
-if __name__ == "__main__":
-    main()
